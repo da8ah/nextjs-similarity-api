9b3c82bc16c8d82fc55921980faec2967a880814
Tiber <danilo.ochoa.hidalgo@outlook.com>
2023-03-29 11:04:38 -0500

    view: Navbar (theme toggle)

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,5 +1,6 @@
 import SignInButton from "@/components/SignInButton";
 import SignOutButton from "@/components/SignOutButton";
+import ThemeToggle from "@/components/ThemeToggle";
 import { buttonVariants } from "@/ui/Button";
 import { getServerSession } from "next-auth";
 import Link from "next/link";
@@ -8,16 +9,18 @@ const Navbar = async () => {
 	const session = await getServerSession();
 
 	return (
-		<div className="fixed backdrop-blur-sm bg-white/75 dark:bg-slate-900 z-50 top-0 left-0 right-0 h-20 border-b boder-slate-300 dark:border-slate-700 shadow-sm flex items-center justify-between">
+		<div className="fixed backdrop-blur-sm bg-white/75 dark:bg-slate-900/75 z-50 top-0 left-0 right-0 h-20 border-b boder-slate-300 dark:border-slate-700 shadow-sm flex items-center justify-between">
 			<div className="container max-w-7xl mx-auto w-full flex justify-between items-center">
 				<Link href="/" className={buttonVariants({ variant: "link" })}>
 					Text Similarity 1.0
 				</Link>
 
-				<div className="md:hidden">{/* <ThemeToggle /> */}</div>
+				<div className="md:hidden">
+					<ThemeToggle />
+				</div>
 
 				<div className="hidden md:flex gap-4">
-					{/* <ThemeToggle /> */}
+					<ThemeToggle />
 					<Link
 						href="/documentation"
 						className={buttonVariants({ variant: "ghost" })}
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,5 @@
-// const { colors } = require("tailwindcss/colors");
-// const { fontFamily } = require("tailwindcss/defaultTheme");
+const { colors } = require("tailwindcss/colors");
+const { fontFamily } = require("tailwindcss/defaultTheme");
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
@@ -12,24 +12,24 @@ module.exports = {
 		// Or if using `src` directory:
 		"./src/**/*.{js,ts,jsx,tsx}",
 	],
-	// theme: {
-	// 	container: {
-	// 		center: true,
-	// 		padding: "1.5rem",
-	// 		screens: {
-	// 			"2xl": "1360px",
-	// 		},
-	// 	},
-	// 	extend: {
-	// 		fontFamily: {
-	// 			sans: ["var(--font-inter)", ...fontFamily.sans],
-	// 		},
-	// 		colors: {
-	// 			...colors,
-	// 			"light-gold": "#f5bc51",
-	// 			"dark-gold": "#533519",
-	// 		},
-	// 	},
-	// },
+	theme: {
+		container: {
+			center: true,
+			padding: "1.5rem",
+			screens: {
+				"2xl": "1360px",
+			},
+		},
+		extend: {
+			fontFamily: {
+				sans: ["var(--font-inter)", ...fontFamily.sans],
+			},
+			colors: {
+				...colors,
+				"light-gold": "#f5bc51",
+				"dark-gold": "#533519",
+			},
+		},
+	},
 	// plugins: [require("tailwindcss-animate"), require("@tailwindcss/typography")],
 };
